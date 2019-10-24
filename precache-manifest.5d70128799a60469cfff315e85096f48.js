462210cc33fc062b6f9fe0020cbdc248bde015ce
Gabe Amaya <gabeamaya2@gmail.com>
2019-10-24 15:44:20 -0500

    Updates

---
+++ precache-manifest.5d70128799a60469cfff315e85096f48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3845a04b3a24b4bd55243604dbb40c12",
+    "url": "/eazy-react-project/index.html"
+  },
+  {
+    "revision": "b44eeda5fa7484b130fb",
+    "url": "/eazy-react-project/static/css/main.48d317f1.chunk.css"
+  },
+  {
+    "revision": "f3be186fa5bd943ee3cd",
+    "url": "/eazy-react-project/static/js/2.41b17ae2.chunk.js"
+  },
+  {
+    "revision": "b44eeda5fa7484b130fb",
+    "url": "/eazy-react-project/static/js/main.dee19303.chunk.js"
+  },
+  {
+    "revision": "00df948f081941509e0b",
+    "url": "/eazy-react-project/static/js/runtime-main.89fec114.js"
+  }
+]);
